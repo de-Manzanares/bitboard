710f4550f381dd6ddc58d82a28aeaf8809d5af46
schauss <138251011+schauss1451@users.noreply.github.com>
2023-07-02 01:42:47 -0700

    added input validation

--- functions/coordinateIndex.cpp
+++ functions/coordinateIndex.cpp
@@ -2,8 +2,18 @@
 
 int coordinateIndex(const std::string& coordinate)
 {
-    int file = 'h' - coordinate[0];
-    int rank = 8 * (coordinate[1] - '1');
+    int index;
 
-    return (rank + file);
+    if ((coordinate[0] < 'a' || coordinate[0] > 'h') ||
+            (coordinate[1] < '1' || coordinate[1] > '8')) {
+        index = 333;
+    }
+    else {
+        int file = 'h' - coordinate[0];
+        int rank = 8 * (coordinate[1] - '1');
+
+        index = rank + file;
+    }
+
+    return index;
 }
