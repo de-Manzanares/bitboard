2cd1ffa7460a0f1efa8e706da6b089a2513fa524
schauss <138251011+schauss1451@users.noreply.github.com>
2023-07-03 16:02:24 -0700

    Update invalid input handling

--- functions/coordinateIndex.cpp
+++ functions/coordinateIndex.cpp
@@ -4,16 +4,15 @@ int coordinateIndex(const std::string& coordinate)
 {
     int index;
 
-    if ((coordinate[0] < 'a' || coordinate[0] > 'h') ||
-            (coordinate[1] < '1' || coordinate[1] > '8')) {
-        index = 333;
+    if (coordinate.length() !=2 ||
+       (coordinate[0] < 'a' || coordinate[0] > 'h') ||
+       (coordinate[1] < '1' || coordinate[1] > '8')) {
+        return 333;
     }
-    else {
-        int file = 'h' - coordinate[0];
-        int rank = 8 * (coordinate[1] - '1');
 
-        index = rank + file;
-    }
+    int file = 'h' - coordinate[0];
+    int rank = 8 * (coordinate[1] - '1');
+    index = rank + file;
 
     return index;
 }
