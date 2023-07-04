aea202d0fa10b58e3f2f9f277d4a544057fecec3
schauss <138251011+schauss1451@users.noreply.github.com>
2023-07-04 12:18:19 -0700

    moved directories renamed from ``removePiece`` to ``subtractPiece``

---
+++ source/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "bitBoards.h"
+
+using namespace std;
+
+int main()
+{
+    string commandLine;
+    ChessBoard board;
+
+    while (getline(cin, commandLine) && commandLine != "quit") {
+        stringstream ss(commandLine);
+        string command;
+        ss >> command;
+
+        if (command == "add") {
+            addReplaceCommand(ss, board);
+        }
+
+        if (command == "d") {
+            printBitBoard(board);
+        }
+
+        if (command == "mv") {
+            moveCommand(ss, board);
+        }
+    }
+    return 0;
+}
+
