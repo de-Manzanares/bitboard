40aedace5aa8a6f3c3f82e9369a5a5c2d478812c
schauss <138251011+schauss1451@users.noreply.github.com>
2023-07-04 12:15:59 -0700

    moved directories
    minor formatting

--- source/coordinateIndex.cpp
+++ source/coordinateIndex.cpp
@@ -5,13 +5,14 @@ int coordinateIndex(const std::string& coordinate)
     int index;
 
     if (coordinate.length() !=2 ||
-       (coordinate[0] < 'a' || coordinate[0] > 'h') ||
-       (coordinate[1] < '1' || coordinate[1] > '8')) {
+        coordinate[0] < 'a' || coordinate[0] > 'h' ||
+        coordinate[1] < '1' || coordinate[1] > '8') {
         return 333;
     }
 
     int file = 'h' - coordinate[0];
     int rank = 8 * (coordinate[1] - '1');
+
     index = rank + file;
 
     return index;
