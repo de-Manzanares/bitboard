e6cdcf6d2a32009ba1d6c2f5ea24440ad531dd8d
schauss <138251011+schauss1451@users.noreply.github.com>
2023-07-09 01:02:36 -0700

    Documentation

--- source/squareSearch.cpp
+++ source/squareSearch.cpp
@@ -1,5 +1,4 @@
-/* 
-    
+/*
     Copyright (C) 2023 de-Manzanares
 
     This program is free software: you can redistribute it and/or modify
@@ -16,44 +15,57 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
     Contact:
-    If you have any questions, comments, or suggestions, 
+    If you have any questions, comments, or suggestions,
     you can reach me at <git.in.touch@dmanz.org>
-
 */
+
+/// @file   squareSearch.cpp
+/// @author de-Manzanares
+/// @brief  Checks a square to determine if it is empty, occupied by a friendly, or occupied by an enemy.
 
 #include <vector>
 #include <string>
 #include "bitBoards.h"
 
-std::vector<int> squareSearch (char pieceFrom, int& flag, int& canMove , int indexTo, ChessBoard& board)
+std::vector<int> squareSearch(char pieceFrom, int& flag, int& canMove, int indexTo, ChessBoard& board)
 {
-    string whitePieces = "KQRBNP";
-    string blackPieces = "kqrbnp";
-    string capturePieces;
-    string collisionPieces;
-    char pieceTo;
+    string whitePieces = "KQRBNP";  // All white pieces
+    string blackPieces = "kqrbnp";  // All black pieces
+    string capturePieces;           // To define which pieces can be captured
+    string collisionPieces;         // To define which pieces can block movement
+    char pieceTo;                   // To store the piece at the destination square
 
-    if (whitePieces.find(pieceFrom) != string::npos) {
-        capturePieces = "kqrbnp";
-        collisionPieces = "KQRBNP";
+    // If the piece to move is white, set the capture pieces to black and the collision pieces to white.
+    // If the piece to move is black, set the capture pieces to white and the collision pieces to black.
+    if (whitePieces.find(pieceFrom)!=string::npos) {
+        capturePieces = blackPieces;
+        collisionPieces = whitePieces;
     }
-    if (blackPieces.find(pieceFrom) != string::npos) {
-        capturePieces = "KQRBNP";
-        collisionPieces = "kqrbnp";
+    if (blackPieces.find(pieceFrom)!=string::npos) {
+        capturePieces = whitePieces;
+        collisionPieces = blackPieces;
     }
 
-    if (!(canMove)) {canMove = false;}
-    if (flag == 2) {canMove = false;}
+    // Using flag from previous squareSearch
+    // If the previous squareSearch says the piece cannot move further, don't bother checking the next square.
+    // If the previous squareSearch says a piece was captured, don't bother checking the next square.
+    if (!(canMove)) { canMove = false; }
+    if (flag==2) { canMove = false; }
 
-    if (flag == 1) {
+    // If the previous squareSearch says the piece can move further, check the next square.
+    // Set flag to 0 if the next square is occupied by a friendly.
+    // Set flag to 1 if the next square is empty.
+    // Set flag to 2 if the next square is occupied by an enemy.
+    // Set canMove to false if the next square is occupied by a friendly.
+    if (flag==1) {
         pieceTo = pieceSearch(indexTo, board);
-        if (collisionPieces.find(pieceTo) != string::npos) {flag = 0;}
-        if (pieceTo == 'E') {flag = 1;}
-        if (capturePieces.find(pieceTo) != string::npos) {flag = 2;}
+        if (collisionPieces.find(pieceTo)!=string::npos) { flag = 0; }
+        if (pieceTo=='E') { flag = 1; }
+        if (capturePieces.find(pieceTo)!=string::npos) { flag = 2; }
 
-        if (flag == 0) {canMove = false;}
-        if (flag == 1) {canMove = true;}
-        if (flag == 2) {canMove = true;}
+        if (flag==0) { canMove = false; }
+        if (flag==1) { canMove = true; }
+        if (flag==2) { canMove = true; }
     }
 
     return {flag, canMove};
